056b23b03ddeb200e0f10ee32c00b2714bc49a8d
Reshav Paul <paulrishav65@gmail.com>
2021-10-12 01:35:56 +0530

    Completed Create/Update Annual Budget

--- src/Year.tsx
+++ src/Year.tsx
@@ -1,45 +1,114 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import { yearlyBudgetType } from './app/constants/Types';
+import { addYearBudget, updateYearBudget } from './app/reducers/yearBudgetReducer';
 import { getYearBudgets } from './app/store';
 import AnnualBudgetForm from './components/AnnualBudgetForm';
 import Titlebar from './components/Titlebar';
+import { faChevronLeft, faChevronRight, faPen } from '@fortawesome/free-solid-svg-icons';
 
 let Year: React.FC<{}> = function (props) {
   let yearBudgets = useSelector(getYearBudgets);
-  let [currentYear, changeCurrentYear] = useState(yearBudgets[0] ? yearBudgets[0].year : (new Date()).getFullYear());
+  let [currentYearIndex, changeCurrentYearIndex] = useState(yearBudgets.length > 0 ? 0 : -1);
+  let [addForm, toggleAddForm] = useState(false);
   let [editForm, toggleEditForm] = useState(false);
 
-  if (yearBudgets.length === 0) {
+  useEffect(() => {
+    if (yearBudgets.length > 0 && currentYearIndex < 0) changeCurrentYearIndex(0);
+    if (currentYearIndex >= yearBudgets.length) changeCurrentYearIndex(yearBudgets.length - 1);
+  }, [currentYearIndex, yearBudgets]);
+
+  let currentYear: number = currentYearIndex > 0 ? yearBudgets[currentYearIndex].year : (new Date()).getFullYear();
+  const dispatch = useDispatch();
+
+  function addNewBudget(payload: yearlyBudgetType) {
+    let existingBudget = yearBudgets.filter(b => b.year === payload.year)[0];
+    if (existingBudget) {
+      let index = currentYearIndex;
+      yearBudgets.some((b, i) => {
+        if (b.year === payload.year) {
+          index = i;
+          return true;
+        }
+        return false;
+      });
+      changeCurrentYearIndex(index);
+      return;
+    }
+    dispatch(addYearBudget(payload));
+  }
+  function updateBudget(payload: yearlyBudgetType) {
+    let existingBudget = yearBudgets.filter(b => b.year === payload.year)[0];
+    if (!existingBudget) {
+      addNewBudget(payload);
+      return;
+    }
+    dispatch(updateYearBudget(payload));
+  }
+  function incrementCurrentYear() {
+    changeCurrentYearIndex((currentYearIndex + 1) % yearBudgets.length);
+  }
+  function decrementCurrentYear() {
+    changeCurrentYearIndex(currentYearIndex === 0 ? yearBudgets.length - 1 : currentYearIndex - 1);
+  }
+
+  if (currentYearIndex < 0) {
     return <div className="container-fluid h-100">
       <Titlebar title={'Annual'} />
-      <AnnualMenuBar onClick={toggleEditForm} year={currentYear} />
+      <AnnualMenuBar onClickAdd={toggleAddForm} year={currentYear} amount={null} mode={'create'}
+        increment={incrementCurrentYear} decrement={decrementCurrentYear} onClickUpdate={toggleEditForm} />
       <div className="container-fluid h-80">
         <div className="d-flex flex-col jc-center ai-center h-100">
           <h4>No Annual Budget Added</h4>
         </div>
       </div>
-      <AnnualBudgetForm close={() => toggleEditForm(false)} active={editForm}
-        mode={'create'} onSubmit={() => { }} budget={{ year: 0, amount: 0 }} />
+      <AnnualBudgetForm close={() => toggleAddForm(false)} active={addForm}
+        mode={'create'} onSubmit={addNewBudget} budget={{ year: currentYear, amount: 0 }} />
     </div>;
   }
 
+  let currentYearBudget = yearBudgets[currentYearIndex];
+
   return <div className="container-fluid h-100">
     <Titlebar title={'Annual'} />
-    <AnnualMenuBar onClick={toggleEditForm} year={currentYear} />
+    <AnnualMenuBar onClickAdd={toggleAddForm} year={currentYearBudget.year} amount={currentYearBudget.amount} mode={'update'}
+      increment={incrementCurrentYear} decrement={decrementCurrentYear} onClickUpdate={toggleEditForm} />
+    <AnnualBudgetForm close={() => toggleAddForm(false)} active={addForm}
+      mode={'create'} onSubmit={addNewBudget} budget={{ year: currentYearBudget.year, amount: 0 }} />
+    <AnnualBudgetForm close={() => toggleEditForm(false)} active={editForm}
+      mode={'update'} onSubmit={updateBudget} budget={currentYearBudget} />
   </div>;
 }
 
 let AnnualMenuBar: React.FC<{
-  onClick: (value: boolean) => void,
-  year: number
+  onClickAdd: (value: boolean) => void,
+  onClickUpdate: (value: boolean) => void,
+  year: number,
+  amount: number | null,
+  mode: 'create' | 'update',
+  increment: () => void,
+  decrement: () => void,
 }> = function (props) {
+
   return <div className="row ai-center">
     <div className="col-10 col-md-9 col-sm-8 text-left">
-      <span>Current Year - {props.year}</span>
+      <FontAwesomeIcon icon={faChevronLeft} className='icon' onClick={props.decrement} />
+      <span className='px-3'>{props.year}</span>
+      <FontAwesomeIcon icon={faChevronRight} className='icon' onClick={props.increment} />
+      <span className="px-3">{props.amount !== null ? 'Budget - ₹ ' + props.amount : ''}</span>
+      {
+        props.mode === 'update' ?
+          <button className="icon-btn ml-3" onClick={e => props.onClickUpdate(true)}>
+            <FontAwesomeIcon className="icon" icon={faPen} />
+            <span className="pl-2 d-sm-none">Update</span>
+          </button>
+          : null
+      }
     </div>
     <div className="col-2 col-md-3 col-sm-4 text-right">
-      <button className="btn" onClick={() => props.onClick(true)}>Add Budget</button>
+      <button className="btn" onClick={() => props.onClickAdd(true)}>Add Budget</button>
     </div>
   </div>;
 }
--- src/app/reducers/yearBudgetReducer.ts
+++ src/app/reducers/yearBudgetReducer.ts
@@ -1,30 +1,40 @@
 import { actionType, yearlyBudgetType } from '../constants/Types';
-import { createReducer, updateItemInArrayByPredicate } from './stateHandlerUtilities';
+import { createReducer, updateItemInArrayByPredicate, updateObject } from './stateHandlerUtilities';
 
 
 const initialState: yearlyBudgetType[] = [];
 
+function addYearlyBudget(state: yearlyBudgetType[], action: actionType) {
+  state = [...state, {
+    year: action.payload.year,
+    amount: action.payload.amount,
+  }];
+  return state;
+}
 
 function updateYearlyBudget(state: yearlyBudgetType[], action: actionType) {
-  const existingBudget: yearlyBudgetType = state.filter(b => b.year === action.payload.year)[0];
-  if (!existingBudget) {
-    state = [...state, {
-      year: action.payload.year,
-      amount: action.payload.amount,
-    }];
-  } else {
-    existingBudget.amount = action.payload.amount;
-    state = updateItemInArrayByPredicate(
-      state,
-      (budget: yearlyBudgetType) => budget.year === action.payload.year,
-      () => existingBudget
-    );
-  }
+  let existingBudget: yearlyBudgetType = state.filter(b => b.year === action.payload.year)[0];
+  let newBudget = updateObject(existingBudget, { amount: action.payload.amount })
+  state = updateItemInArrayByPredicate(
+    state,
+    (budget: yearlyBudgetType) => budget.year === action.payload.year,
+    () => newBudget
+  );
   return state;
 }
 
 const yearBudgetReducer = createReducer(initialState, {
+  'budget/year/add': addYearlyBudget,
   'budget/year/update': updateYearlyBudget,
 });
+
+// action creators
+export function addYearBudget(payload: any): actionType {
+  return { type: 'budget/year/add', payload: payload };
+}
+
+export function updateYearBudget(payload: any): actionType {
+  return { type: 'budget/year/update', payload: payload };
+}
 
 export default yearBudgetReducer;
