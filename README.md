bb33bb4b5c57b20ecb7d344c4fc0c4c9e8b43a08
Reshav Paul <paulrishav65@gmail.com>
2022-03-27 13:13:53 +0530

    Started new Frontend App

--- README.md
+++
@@ -1,6 +0,0 @@
-# ExSpender - The Expense Tracker App
-
-Objectives - 
-1. UI with React and Redux
-2. Backend with ExpressJS
-3. Visualization with D3.js
--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Loading: React.FC<{}> = function (props) {
-    return <div className="h-100 text-center d-flex flex-col ai-center jc-center">
-        <h4>Loading...</h4>
-    </div>
-}
-
-export default Loading;
